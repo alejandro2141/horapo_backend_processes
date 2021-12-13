fe1726777db738de7254edd48f8121aff01372f2
alejandro2141 <alejandro2141@gmail.com>
2021-12-13 19:42:20 -0300

    end version1 001

--- 0001_sendConfirmationAppointmentTaken.js
+++ 0001_sendConfirmationAppointmentTaken.js
@@ -2,6 +2,17 @@ const { Pool, Client } = require('pg')
 
 console.log("0001 Send email confirmation Appointment Reserved");
 
+let conString = {
+  user: 'conmeddb_user',
+  host: '127.0.0.1',
+  database: 'conmeddb02',
+  password: 'paranoid',
+  port: 5432,
+    }
+
+const client = new Client(conString) ; 
+
+/*
 const client = new Client({
   user: 'conmeddb_user',
   host: '127.0.0.1',
@@ -9,17 +20,103 @@ const client = new Client({
   password: 'paranoid',
   port: 5432,
     })
+*/
 
 client.connect()
 // ****** Run query to bring appointment
-const sql  = "SELECT * from  appointment where  app_reserved_notification_status is NULL  AND app_blocked = 99 AND app_available = false ;" ;
-console.log('---> QUERY : '+sql ) ;
-const resultado = client.query(sql, (err, result) => {
+const sql  = "SELECT * from  appointment WHERE  patient_notification_email_reserved = 0  AND app_blocked = 0 AND app_available = false  " ;
 
+console.log('---> QUERY : '+sql ) ;
+
+const resultado = client.query(sql, (err, result) => {
   if (err) {
-      console.log('get_professional_specialty ERR:'+err ) ;
+      console.log('ERR:'+err ) ;
     }
-    client.end()
+  if (result != null)
+    {  
+      console.log("Registers Found:" +result.rows.length );
+    //console.log("result in function:"+JSON.stringify(result.rows));
+      if (result.rows.length >0 ){
+           appToNotifyReserved(result.rows); }
+      else {
+        console.log("Empty List, No new Registers");
+      }
+    
+    }
+    client.end() ;
+  })
+
+  function appToNotifyReserved(list)
+  {
+    console.log("continueToUpdate function. "+JSON.stringify(list));
+    console.log("Total EAMILS to be send : "+list.length );
+     
+    for ( i=0 ; i<= list.length ; i++ )
+    {
+      val=list.pop() ;
+      console.log("Sending email to :"+ val.patient_email  );
+//****************** */
+      let message="Estimad@ <b>"+val.patient_name+"</b> <br> Su reserva para [ESPECIALIDAD] ha sido registrada el dia:"+val.date+ " a las :"+val.start_time+" ha sido generada<br> Recuerde debe confirmar su asistencia 48 horas antes de la cita, de lo contrario su hora sera liberada para otros pacientes" ;
+
+      var nodemailer = require('nodemailer');
+      var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+          user: 'ing.morales@gmail.com',
+          pass: 'cshbjfqevdyceras'
+          }
+      });
+      
+      var mailOptions = {
+          from: 'ing.morales@gmail.com',
+          to: val.patient_email ,
+          subject: 'Cita de ESPECIALIDAD ha sido reservada para:'+val.date+', a las:'+val.start_time+'  ',
+          text: 'Gracias por preferirnos '+val.patient_name ,
+          html: message , 
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+          console.log(error);
+          } else {
+          console.log('Email sent: ' + info.response);
+          updateRegisterToNotified(val) ;
+               }
+        })
+    
+    }
+
+}
+
+
+function updateRegisterToNotified(val)
+{
+ //************ */
+ //client.connect()
+ 
+ let sql_update  = "UPDATE appointment SET patient_notification_email_reserved = 1  WHERE id="+val.id+" ;" ;
+ console.log('--->UPDATE APP QUERY : '+sql_update ) ;
+
+ const client_update = new Client(conString) ;
+ client_update.connect() ;
+ const resultado = client_update.query(sql_update, (err, result) => {
+  if (err) {
+      console.log('ERR:'+err ) ;
+    }
+    console.log("updated Result" + result);
+    client_update.end() ;
+  })
+
+ //*************** */
+}
+
+
+// CLOSE CLIENT
+ 
+
+//console.log("LIST="+JSON.stringify(list) ) ;
+
+  /*
 //  console.log('---> Emails Pending to send  : '+JSON.stringify(result.rows) ) ;
   console.log("START SUMMARY EMAILS");
   result.rows.forEach( (value)=>console.log("all :"+JSON.stringify(value)) );
@@ -54,6 +151,20 @@ const resultado = client.query(sql, (err, result) => {
                 console.log(error);
                 } else {
                 console.log('Email sent: ' + info.response);
+
+                //Now we should update DB to reflect email sent. 
+                client.connect()
+                const sql  = "UPDATE appointment SET patient_notification_email_reserved = 1  WHERE id=1385 ;" ;
+                console.log('--->UPDATE APP QUERY : '+sql ) ;
+                const resultado = client.query(sql, (err, result) => {
+                  if (err) {
+                      console.log('get_professional_specialty ERR:'+err ) ;
+                    }
+                client.end() ;
+                 
+                //END UPDATE DATABASE                 
+
+
                 }
             });
 
@@ -67,4 +178,6 @@ const resultado = client.query(sql, (err, result) => {
 
  
 })
+
+*/
 
