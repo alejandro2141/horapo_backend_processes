2d475f34e65eca2787e1796134f0bfce41e26d92
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-01-09 17:51:47 -0300

    Update 0001_sendConfirmationAppointmentTaken.js

--- 0001_sendConfirmationAppointmentTaken.js
+++ 0001_sendConfirmationAppointmentTaken.js
@@ -74,13 +74,7 @@ const resultado = client.query(sql, (err, result) => {
 
       // create Nodemailer SES transporter
       let transporter = nodemailer.createTransport({
-              host: "email-smtp.us-east-2.amazonaws.com",
-              port: 587,
-              secure: false, // true for 465, false for other ports
-              auth: {
-                user: 'AKIAZX6HYCD6TMLDYJK4' , // generated ethereal user
-                pass: '4XYTAIwLLF3tJSktdv1K2JRA8ASu9xgvms0Nk8sO', // generated ethereal password
-              },
+        SES: { ses, aws },
       });
 
       // send some mail
