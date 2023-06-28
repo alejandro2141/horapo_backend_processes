0c8005d48fcee0e77a1ee905c52a69d74f1c5b56
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-06-28 17:36:36 -0400

    Update 0004_sendCalendarToPatient.js

--- 0004_sendCalendarToPatient.js
+++ 0004_sendCalendarToPatient.js
@@ -114,7 +114,7 @@ if (email_list != null && email_list.length > 0 )
 }
 catch (e)
 {
-  console.log(cdate.toLocaleString()+":S0004:CATCH ERROR PROCESS EXIT:"+info);
+  console.log(cdate.toLocaleString()+":S0004:CATCH ERROR PROCESS EXIT:"+e);
   process.exit()
 }
 
